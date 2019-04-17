a89c47d490fa84215abc4956e6f5230cd5ff1ef9
Sergio Ramirez <sergioestebanramirez@yahoo.es>
2019-04-16 23:45:45 -0500

    Escenario de preguntados creado
    Se creó el escenario de preguntados y se añadieron algunas opciones para posteriormente hacer click sobre ellas y mover la cámara

---
+++ Proyecto Practica/Assets/Scripts/moverCamara.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moverCamara : MonoBehaviour
+{
+    //public Camera mn;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+     //Camera.main.transform.Translate(Input.GetAxisRaw("Horizontal") * 10, Input.GetAxisRaw("Vertical") * 10, 0);
+       Camera.main.transform.Translate(Input.GetAxisRaw("Horizontal") * 1, Input.GetAxisRaw("Vertical") * 1, 0);
+        
+    }
+}
