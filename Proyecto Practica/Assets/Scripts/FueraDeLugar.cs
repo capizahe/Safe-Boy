1ef919e2d8e2b90f27c473dfcffb34181b6dcc41
camilo <camiloandrespiza@gmail.com>
2019-04-18 14:55:28 -0500

    Script para mantener al personaje dentro de la escena nivel 1
    Si el personaje sale de la escena, se posicionara nuevamente en el lugar.

---
+++ Proyecto Practica/Assets/Scripts/FueraDeLugar.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FueraDeLugar : MonoBehaviour
+{
+
+    public GameObject player;
+    private bool isIn;
+
+    void Start()
+    {
+        this.isIn = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!check())
+        {
+            restartPosition();
+        }
+    }
+
+    /*
+     *Este metodo verifica que el personaje no haya salido por la ventana 
+     */
+    bool check()
+    {
+        this.isIn = (player.transform.position.y < -5 && player.transform.position.x > 8) ? false : true;
+        return this.isIn;
+    }
+    /*
+     *Este metodo pondra el personaje nuevamente en su posicion inicial. 
+     */
+    void restartPosition()
+    {
+        player.transform.position = new Vector3(-6.98f, -3f, -3.469688f);
+        this.isIn = true;
+    }
+}
