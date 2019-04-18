91cf8b95a8e20197491edc31bcd9928fcde52577
Sergio Ramirez <sergioestebanramirez@yahoo.es>
2019-04-17 23:49:07 -0500

    Avance menú de preguntados
    Se configuraron botones para mover camara en la escena

--- Proyecto Practica/Assets/Scripts/moverCamara.cs
+++ Proyecto Practica/Assets/Scripts/moverCamara.cs
@@ -15,7 +15,25 @@ public class moverCamara : MonoBehaviour
     void Update()
     {
      //Camera.main.transform.Translate(Input.GetAxisRaw("Horizontal") * 10, Input.GetAxisRaw("Vertical") * 10, 0);
-       Camera.main.transform.Translate(Input.GetAxisRaw("Horizontal") * 1, Input.GetAxisRaw("Vertical") * 1, 0);
+     //Camera.main.transform.Translate(Input.GetAxisRaw("Horizontal") * 1, Input.GetAxisRaw("Vertical") * 1, 0);   
+    }
+
+    void OnMouseUp()
+    {
+        Debug.Log(this.name);
         
+
+        Vector3 targetPosition = new Vector3(13.4f, 0, 0);
+        
+        if (this.name.Equals("Caution"))
+            Camera.main.transform.Translate(13.4f ,0,0);
+
+        if (this.name.Equals("faid"))
+            Camera.main.transform.Translate(0, 10 , 0);
+
+        if (this.name.Equals("terremoto"))
+            Camera.main.transform.Translate(0, -10, 0);
+
+
     }
 }
