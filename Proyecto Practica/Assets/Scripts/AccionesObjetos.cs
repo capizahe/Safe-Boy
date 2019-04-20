f4aa340ab2c51bd011663f17a6ee87c85a6fb901
camilo <camiloandrespiza@gmail.com>
2019-04-20 08:45:24 -0500

    Interaccion Objectos con teclas

---
+++ Proyecto Practica/Assets/Scripts/AccionesObjetos.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AccionesObjetos 
+{
+    private Text text;
+    private string tag;
+    private GameObject player;
+    private GameObject currentObject;
+
+    public AccionesObjetos(string tag, GameObject player, GameObject currentObject,Text text) 
+    {
+        this.text = text;
+        this.tag = tag;
+        this.player = player;
+        this.currentObject = currentObject;
+    }
+
+    public void checkTag()
+    {
+
+        if (this.tag.Equals("fuego")){restartPosition();}
+
+    }
+
+    public bool isGrabbable(string tag)
+    {
+        if (tag.Equals("fuego")) return false;
+        return true;
+    }
+
+    private void restartPosition()
+    {
+        player.transform.position = new Vector3(4.82f, -2.57f, -3.469688f);
+        text.text = "Objeto = [Vacio]";
+    }
+
+    
+
+
+}
--- Proyecto Practica/Assets/Scripts/InteracionObjetos.cs
+++ Proyecto Practica/Assets/Scripts/InteracionObjetos.cs
@@ -16,35 +16,71 @@ public class InteracionObjetos : MonoBehaviour
 
     public Text texto;
     public GameObject player;
+    public GameObject currentObject = null;
+    private bool hasObject = false;
+    private GameObject grabbedObject=null;
+    private AccionesObjetos accionesObjetos= null;
 
 
     void Start()
     {
+        texto.text = "Objeto = [Vacio]";
+    }
+
+     void Update()
+    {
+
+        if(Input.GetKey(KeyCode.Space) && this.currentObject && !this.hasObject && !this.grabbedObject)
+        {
+            if (accionesObjetos.isGrabbable(this.currentObject.tag))
+            {
+                this.hasObject = true;
+                this.currentObject.SetActive(false);
+                this.grabbedObject = this.currentObject;
+                Debug.Log("The grabbed object is -> " + this.currentObject.tag);
+            }
+            else
+            {
+                Debug.Log("The object -> " + this.currentObject.tag + " is not grabbable.");
+            }
+        }
+        if (Input.GetKey(KeyCode.Z) && this.hasObject && this.grabbedObject)
+        {
+            this.grabbedObject = null;
+            this.currentObject.SetActive(true);
+            this.currentObject = null;
+            this.hasObject = false;
+            texto.text = "Objeto = [Vacio]";
+        }
+
 
     }
 
     void OnTriggerEnter2D(Collider2D col)
     {
-        if(col.tag == "Extintor")
-        {
-            texto.text = col.tag;
-        }
-        else if (col.tag == "fuego")
-        {
-            restartPosition();
-        }
+        this.currentObject = col.gameObject;
+
+         texto.text = " Objeto = ["+col.tag+"]";
+
+        accionesObjetos = new AccionesObjetos(col.tag, this.player, this.currentObject, this.texto);
+
+        accionesObjetos.checkTag();
+
+
+
+
+
     }
     void OnTriggerExit2D(Collider2D col)
     {
-        if (col.tag == "Extintor")
+        if (this.currentObject != null)
         {
-            texto.text = "";
+            this.currentObject = null;
         }
+       
+            texto.text = "Objeto = [Vacio]";
     }
-    void restartPosition()
-    {
-      player.transform.position = new Vector3(4.82f, -2.57f, -3.469688f);
-    }
+   
 
 
 }
